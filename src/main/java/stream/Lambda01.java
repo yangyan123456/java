67cfb83c89c70dad40fb891807d9fc59a26ff359
yyq <940484974@qq.com>
2024-06-16 14:15:35 +0800

    Changes

--- src/main/java/stream/Lambda01.java
+++ src/main/java/stream/Lambda01.java
@@ -1,2 +1,15 @@
-package stream;public class Lambda01 {
+package stream;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Lambda01 {
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                log.info("=======hello======");
+            }
+        }).start();
+    }
 }
