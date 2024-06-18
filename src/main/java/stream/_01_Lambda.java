97f355c546d06bc7ad7a4db6802ac4b6b203f02a
yyq <940484974@qq.com>
2024-06-19 01:20:25 +0800

    Update .gitignore to exclude gradle folder

---
+++ src/main/java/stream/_01_Lambda.java
@@ -0,0 +1,23 @@
+package stream;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.function.IntBinaryOperator;
+
+@Slf4j
+public class _01_Lambda {
+
+    public static  int calculateNum(IntBinaryOperator operator) {
+        int a = 10;
+        int b = 20;
+        return operator.applyAsInt(a,b);
+    }
+
+
+    public static void main(String[] args) {
+        new Thread(() -> log.info("=======hello======")).start();
+
+        int res = calculateNum((left, right) -> left * right);
+        log.info("=======res = " + res + "===========");
+    }
+}
